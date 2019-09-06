ab186eeaa2d9323abc9575c3851b5a1318db3725
duzhenduo <duzhenduo@126.com>
2019-09-06 09:31:57 +0800

    对账定时任务，以光大银行对账文件为准，渠道多（核心记账成功）的退款

--- cap-ceba-service/db/ceba_settle_log_oracle.sql
+++ cap-ceba-service/db/ceba_settle_log_oracle.sql
@@ -5,7 +5,8 @@ tx_sts VARCHAR2(1) NOT NULL ,
 cnaps_date NUMBER(11) NULL ,
 cnaps_traceno VARCHAR2(20) NULL ,
 cnaps_code VARCHAR2(20) NULL ,
-cnaps_msg VARCHAR2(100) NULL
+cnaps_msg VARCHAR2(100) NULL,
+tx_amt NUMBER(16,2) NULL
 );
 COMMENT ON TABLE ceba_settle_log IS '光大云缴费清算流水表';
 COMMENT ON COLUMN ceba_settle_log.chk_date IS '对账日期';
@@ -14,5 +15,6 @@ COMMENT ON COLUMN ceba_settle_log.cnaps_date IS '二代记账日期';
 COMMENT ON COLUMN ceba_settle_log.cnaps_traceno IS '二代记账流水号';
 COMMENT ON COLUMN ceba_settle_log.cnaps_code IS '二代记账响应码';
 COMMENT ON COLUMN ceba_settle_log.cnaps_msg IS '二代记账响应信息';
+COMMENT ON COLUMN ceba_settle_log.tx_amt IS '交易金额';
 
 ALTER TABLE ceba_settle_log ADD PRIMARY KEY (chk_date);
